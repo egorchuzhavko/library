5ae4b1c7bd2a53d7c52e8570c02e7cf26b5d0e3d
mentallyillakilla <76404691+egorchuzhavko@users.noreply.github.com>
2021-12-27 11:18:22 +0300

    Добавьте файлы проекта.

---
+++ IntervalTime/TimeInterval.cs
@@ -0,0 +1,35 @@
+﻿namespace IntervalTime
+{
+    public class TimeInterval
+    {
+        private readonly double sec;
+        public double Tick { get { return sec * 18.2; } }
+        public double Min { get { return sec / 60; } }
+
+        private TimeInterval(double sec)
+        {
+            this.sec = sec;
+        }
+        public static TimeInterval MMin(double m)
+        {
+            return new TimeInterval(m * 60);
+        }
+
+        public static TimeInterval TTick(double tick)
+        {
+            return new TimeInterval(tick / 18.2);
+        }
+
+        public TimeInterval Addiction(TimeInterval other)
+        {
+            return new TimeInterval(this.sec + other.sec);
+        }
+
+        public TimeInterval Substraction(TimeInterval other)
+        {
+            return new TimeInterval(this.sec - other.sec);
+        }
+
+        public override string ToString() { return $"Минут: {Min}\nСекунд: {sec}\nТик: {Tick}"; }
+    }
+}
---
+++ library/Program.cs
@@ -0,0 +1,10 @@
+﻿using IntervalTime;
+
+TimeInterval first = TimeInterval.MMin(30);
+TimeInterval second = TimeInterval.TTick(30);
+Console.WriteLine(first.ToString() + "\n");
+Console.WriteLine(second.ToString() + "\n");
+TimeInterval third = first.Addiction(second);
+Console.WriteLine("Сложение 1 на 2\n" + third.ToString() + "\n");
+third = second.Substraction(first);
+Console.WriteLine("Вычитание 2 на 1\n" + third.ToString() + "\n");
